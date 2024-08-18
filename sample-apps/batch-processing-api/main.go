6174f57317ae04d3c34970e01ccba27a77eb25e8
Erik Kvale <eirikval@gmail.com>
2024-08-17 22:52:42 -0600

    Added sample-apps dir and initialized an API based batch processing program

---
+++ sample-apps/batch-processing-api/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	// Create a new Echo instance
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/health", func(c echo.Context) error {
+		return c.String(http.StatusOK, "API is up and running!")
+	})
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
